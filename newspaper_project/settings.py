c086a77f39b4833e36754f3d2aa3552646fff470
DhruvDiwan <dhruv18031@iiitd.ac.in>
2019-06-01 17:40:57 +0530

    New updates for heroku deployment

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$lnr5y6ol@w%$0xk$5+4sep$&@vyqaxwkfn-&)r)zpaqk$()_a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
